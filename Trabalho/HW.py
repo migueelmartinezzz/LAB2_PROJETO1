2458a26c70a7046e196063224bd0b2de38ab68c3
Miguel Silva Martinez <B435646@fgv.edu.br>
2024-08-12 18:44:00 -0300

    Acrescentando função que sorteia números e dá a média

--- Trabalho/HW.py
+++ Trabalho/HW.py
@@ -1,5 +1,12 @@
 def cumprimento(texto):
     return f"Olá {texto}"
 print(cumprimento("Miguel Silva Martinez"))
+import random
 
+def calcular_media_3numeros():
+    # Sortear 3 números aleatórios entre 1 e 100
+    numeros = [random.randint(1, 100) for _ in range(3)]
+    # Calculando a média dos números
+    media = sum(numeros) / len(numeros)
+    return media
                   
